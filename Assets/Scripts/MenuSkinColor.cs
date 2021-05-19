d1dd94fcff3926fa9aa6a20b241024f53a94c245
tshuenhau <tshuenhau@gmail.com>
2021-05-19 23:08:57 +0800

    updated main menu title and added icons

--- Assets/Scripts/MenuSkinColor.cs
+++ Assets/Scripts/MenuSkinColor.cs
@@ -19,8 +19,9 @@ public class MenuSkinColor : MonoBehaviour
     float colorChangeTimer;
 
     // Start is called before the first frame update
-    void Awake(){
-        
+    void Awake()
+    {
+
     }
     void Start()
     {
@@ -29,10 +30,12 @@ public class MenuSkinColor : MonoBehaviour
         image = GetComponent<Image>();
         colorChangeTimer = timerCount;
         image.color = colorList[colorListIndex];
-        if(colorListIndex+1 < colorList.Count-1){
+        if (colorListIndex + 1 < colorList.Count - 1)
+        {
             colorListIndex++;
         }
-        else{
+        else
+        {
             colorListIndex = 0;
         }
     }
@@ -41,44 +44,55 @@ public class MenuSkinColor : MonoBehaviour
     void Update()
     {
         colorChangeTimer -= Time.deltaTime;
-        if(colorChangeTimer <= 0){
-            if(colorListIndex < colorList.Count){
+        if (colorChangeTimer <= 0)
+        {
+            if (colorListIndex < colorList.Count)
+            {
                 image.color = nextColor;
                 nextColor = colorList[colorListIndex];
-                if(colorListIndex == colorList.Count-1){
-                        colorListIndex = 0;
-                    }
-                else{
+                if (colorListIndex == colorList.Count - 1)
+                {
+                    colorListIndex = 0;
+                }
+                else
+                {
                     colorListIndex++;
                 }
             }
-            colorChangeTimer = timerCount;      
+            colorChangeTimer = timerCount;
         }
 
-        foreach(Transform eye in eyes.transform){
-            eye.GetComponent<Image>().color = image.color; 
+        foreach (Transform eye in eyes.transform)
+        {
+            eye.GetComponent<Image>().color = image.color;
         }
     }
-
-    public void SetColor(){
+    //TODO colorIndex might need to be adjusted like in ColorController
+    public void SetColor()
+    {
         colorList.Clear();
-        if(currentLevel >= 0){ // level 0
-            colorList.Add(new Color(0.6078432f,0.9647059f,1f)); //Blue
+        if (currentLevel >= 0)
+        { // level 0
+            colorList.Add(new Color(0.6078432f, 0.9647059f, 1f)); //Blue
             currentColor = colorList[colorListIndex];
             nextColor = colorList[colorListIndex];
-            if(currentLevel >= 4){ // level 3
-                colorList.Add(new Color(0.7921569f,1,0.7490196f)); //Green
-                if(currentLevel >= 15){ // level 12
-                    colorList.Add(new Color(1,0.6784314f,0.6784314f));//Red
-                    if(currentLevel >= 24){ // level 24
+            if (currentLevel >= 4)
+            { // level 3
+                colorList.Add(new Color(0.7921569f, 1, 0.7490196f)); //Green
+                if (currentLevel >= 15)
+                { // level 12
+                    colorList.Add(new Color(1, 0.6784314f, 0.6784314f));//Red
+                    if (currentLevel >= 24)
+                    { // level 24
                         colorList.Add(new Color(0.9921569f, 1, 0.7137255f)); //yellow
-                        if(currentLevel >= 36){ // level 36
-                            colorList.Add(new Color(1,0.7764706f,1));
+                        if (currentLevel >= 36)
+                        { // level 36
+                            colorList.Add(new Color(1, 0.7764706f, 1));
                         }
                     }
                 }
                 currentColor = colorList[colorListIndex];
-                nextColor = colorList[colorListIndex+1];
+                nextColor = colorList[colorListIndex + 1];
             }
         }
     }
